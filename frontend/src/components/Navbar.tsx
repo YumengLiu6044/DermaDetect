943d2c1410936c93631fb65387857a34ca9a4382
Yumeng Liu <lym20041026@gmail.com>
2025-04-17 22:38:47 -0700

    no underline

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
 	return (
-		<div className="flex gap-5 h-16 w-full px-25 items-center bg-[#F5F5F5]">
+		<div className="flex gap-5 h-16 w-full px-20 items-center bg-[#F5F5F5]">
 			<div className="flex items-center">
 				<i className="fas fa-dna text-indigo-600 text-2xl mr-2"></i>
 				<span className="text-xl font-bold text-black">
@@ -12,28 +12,28 @@ function Navbar() {
 			<div className="w-full flex justify-baseline sm:ml-6 sm:flex sm:space-x-8">
 				<Link
 					to="/"
-					className="text-black no-underline hover:text-black inline-flex items-center px-1 pt-1 font-medium text-sm"
-					style={{ textDecoration: "none" }}
+					className="navbar-link text-black no-underline hover:text-black inline-flex items-center px-1 pt-1 font-medium text-sm"
+                    style={{ textDecoration: "none" }}
 				>
 					Home
 				</Link>
 				<Link
 					to="/how-it-works"
-					className="text-black hover:text-black inline-flex items-center px-1 pt-1 font-medium text-sm"
+					className="navbar-link text-black hover:text-black inline-flex items-center px-1 pt-1 font-medium text-sm"
 					style={{ textDecoration: "none" }}
 				>
 					How It Works
 				</Link>
 				<Link
 					to="/about-skin-cancer"
-					className="text-black hover:text-black inline-flex items-center px-1 pt-1 font-medium text-sm"
+					className="navbar-link text-black hover:text-black inline-flex items-center px-1 pt-1 font-medium text-sm"
 					style={{ textDecoration: "none" }}
 				>
 					About Skin Cancer
 				</Link>
 				<Link
 					to="/find-a-dermatologist"
-					className="text-black hover:text-black inline-flex items-center px-1 pt-1 font-medium text-sm"
+					className="navbar-link text-black hover:text-black inline-flex items-center px-1 pt-1 font-medium text-sm"
 					style={{ textDecoration: "none" }}
 				>
 					Find a Dermatologist
