a348edf0f09b122c9e55a8e3c4fe3c9e7cb95728
Yumeng Liu <lym20041026@gmail.com>
2025-04-20 13:25:24 -0700

    model

--- backend/model.py
+++ backend/model.py
@@ -5,15 +5,15 @@ class DermaScannerModel(nn.Module):
     def __init__(self, num_classes=7):
         super(DermaScannerModel, self).__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(3, 16, kernel_size=3, padding=1),  # (64, 64)
+            nn.Conv2d(3, 16, kernel_size=5, padding=2),  # (64, 64)
             nn.ReLU(),
             nn.MaxPool2d(2),  # -> (32, 32)
 
-            nn.Conv2d(16, 32, kernel_size=3, padding=1),  # (32, 32)
+            nn.Conv2d(16, 32, kernel_size=5, padding=2),  # (32, 32)
             nn.ReLU(),
             nn.MaxPool2d(2),  # -> (16, 16)
 
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),  # (16, 16)
+            nn.Conv2d(32, 64, kernel_size=5, padding=2),  # (16, 16)
             nn.ReLU(),
             nn.MaxPool2d(2),  # -> (8, 8)
         )
