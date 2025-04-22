cc4b125322a97b4ed461947065ebb8e9a2833113
Yumeng Liu <lym20041026@gmail.com>
2025-04-21 20:04:12 -0700

    updated endpoint

--- frontend/src/utility/fetch.ts
+++ frontend/src/utility/fetch.ts
@@ -1,5 +1,5 @@
-// const API_ENDPOINT = "https://dermadetect.onrender.com/";
-const API_ENDPOINT = "http://127.0.0.1:8080/";
+const API_ENDPOINT = "https://dermadetect.onrender.com/";
+// const API_ENDPOINT = "http://127.0.0.1:8080/";
 
 export type DiseaseProbs = {
 	"Benign keratosis-like lesions": number;
