38a67c5ad82dcafc213fa343dc1539a4f3ba39c5
Yumeng Liu <lym20041026@gmail.com>
2025-05-04 09:02:37 -0700

    update found derma length

--- frontend/src/pages/FindADermatologist.tsx
+++ frontend/src/pages/FindADermatologist.tsx
@@ -183,7 +183,7 @@ export default function FindADermatologist() {
 					) : renderedDocResults.length > 0 ? (
 						<div className="flex flex-col gap-2 w-full overflow-y-auto">
 							<span className="text-black text-2xl font-medium">
-								Found {docResults.length} Dermatologists
+								Found {renderedDocResults.length} Dermatologists
 							</span>
 
 							{renderedDocResults.map((item, index) => (
